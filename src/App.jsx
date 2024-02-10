8983fc29e0ea0f87fba911e597d5a75753e85207
Mahad <mahad9036@gmail.com>
2024-02-10 15:22:05 +0500

    Adding dynamic features like add task and delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,73 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todo, setTodo] = useState([])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodo((currentTodos) => {
+      return [...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false }]
+    })
+    setNewItem("") // Clear the input field after submitting
+  }
+
+  function toggleTodo(id,completed){
+    setTodo(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodo(currentTodo => {
+      return currentTodo.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
-<>
-<div class="bg-gray-800 min-h-screen py-8">
-  <div class="max-w-md mx-auto">
-    <form class="mb-4">
-      <label class="text-white">New item</label><br/>
-      <input type='text' placeholder='Write task' class="border border-blue-500 rounded-md bg-blue-100 px-4 py-2 w-full mb-2 text-gray-800"></input><br/>
-      <button class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Add</button>
-    </form>
-    <div>
-      <h2 class="text-xl font-semibold text-white mb-2">Tasks:</h2>
-      <ul>
-        <li class="flex items-center justify-between bg-gray-700 rounded-md px-4 py-2 mb-2">
-          <label class="flex items-center text-white">
-            <input type='checkbox' class="mr-2"></input>
-            Item 1
-          </label>
+    <>
+      <div className="bg-gray-800 min-h-screen py-8">
+        <div className="max-w-md mx-auto">
+          <form onSubmit={handleSubmit} className="mb-4">
+            <label className="text-white">New item</label><br/>
+            <input 
+              type='text' 
+              value={newItem}
+              onChange={e => setNewItem(e.target.value)}
+              className="border border-blue-500 rounded-md bg-blue-100 px-4 py-2 w-full mb-2 text-gray-800"
+            /><br/>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Add</button>
+          </form>
           <div>
-            <button class="border border-green-500 text-green-500 px-3 py-1 rounded-md">Update</button>
-            <button class="border border-red-500 text-red-500 px-3 py-1 ml-3 rounded-md">Delete</button>
+            <h2 className="text-xl font-semibold text-white mb-2">Tasks:</h2>
+            <ul>
+              {todo.map(todoItem => {
+                return (
+                  <li key={todoItem.id} className="flex items-center justify-between bg-gray-700 rounded-md px-4 py-2 mb-2">
+                    <label className="flex items-center text-white">
+                      <input type='checkbox'
+                      onChange={e => toggleTodo(todoItem.id, e.target.checked)}
+                      checked={todoItem.completed} className="mr-2"></input>
+                      {todoItem.title}
+                    </label>
+                    <div>
+                      {/* <button class="border border-green-500 text-green-500 px-3 py-1 rounded-md">Update</button> */}
+                      <button onClick={() => deleteTodo(todoItem.id)} className="border border-red-500 text-red-500 px-3 py-1 ml-3 rounded-md">Delete</button>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-</>
-
-
+        </div>
+      </div>
+    </>
   )
 }
 
